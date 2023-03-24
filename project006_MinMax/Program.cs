487b223b985b9f631d57d9a987d5f084175f764d
ReZero <ghostofpast@icloud.com>
2023-03-24 14:59:58 +0300

    add MinMax pj

---
+++ project006_MinMax/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = new Random().Next(1,10000);
+int b = new Random().Next(1,10000);
+int c = new Random().Next(1,10000);
+int d = new Random().Next(1,10000);
+int e = new Random().Next(1,10000);
+int f = new Random().Next(1,10000);
+int g = new Random().Next(1,10000);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+if (g > max) max = g;
+
+Console.Write("max = ");
+Console.WriteLine(max);
