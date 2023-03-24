593c04755c38c5860733c77ff19866d57b044755
ReZero <ghostofpast@icloud.com>
2023-03-24 14:43:22 +0300

    add IfElse pj

---
+++ project005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "стас")
+{
+    Console.WriteLine("Привет, создатель!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
